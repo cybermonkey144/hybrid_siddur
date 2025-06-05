c6e953f969daae05745b481cc9f34032255d5372
Clark Kent <icreatemyweb@Gmail.com>
2025-06-05 18:07:18 +0300

    Fixes

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 const AVAILABLE_NUSACHIM = [
     { id: "ashkenaz", name: "Ashkenaz (אשכנז)" },
+    { id: "ashkenaz_temp", name: "Ashkenaz (אשכנז) - temp" },
     { id: "sephardic", name: "Sephardic (ספרדי)" },
     { id: "edot_hamizrach", name: "Edot HaMizrach (עדות המזרח)" }
     // Add more Nusachim here
 ];
 
 const PRAYER_DEFINITIONS = [
-    { id: "shacharit", name: "Shacharit (שחרית)", template: "data/templates/shacharit_template.json" },
+    { id: "shacharit", name: "Shacharit (שחרית) -Beta-", template: "data/templates/shacharit_template.json" },
     { id: "mincha", name: "Mincha (מנחה)", template: "data/templates/mincha_template.json" }
     // Add more prayer definitions here
 ];
